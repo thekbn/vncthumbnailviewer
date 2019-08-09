be573856999ad3e0fb2df90bff2265cec08e9d06
KBN Saifullah <thekbn@gmail.com>
2019-08-09 17:37:57 -0400

    updated script to automaticall grab ports from docker

--- view.sh
+++ view.sh
@@ -1,34 +1,58 @@
-echo "starting VNC Thunbnail Viewer"
-echo $ports
+usage(){
+     cat <<HELP_USAGE
 
-host=localhost
-portRangeStart=5900
-portRangeEnd=5909
-password=secret
+    Usage: $0 [-h|--host host] [-p|--port ports...] [--pass password]
+        -h, --host  specify host
+        -p, --port  specify on or more ports
+            --pass  specify password
 
-if [ ! -z $1 ]
-then
-    host=$1
-fi
+HELP_USAGE
+}
 
-if [ ! -z $2 ]
-then
-    portRangeStart=$2
-fi
+#set default values
+HOST=localhost
+PASS=secret
 
-if [ ! -z $3 ]
-then
-    portRangeEnd=$3
-fi
-
-if [ ! -z $4 ]
-then
-    password=$4
-fi
-
-for i in $(seq $portRangeStart $portRangeEnd); 
-do 
-    params="$params HOST $host PORT $i PASSWORD $password"
+#get options
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -h|--host)
+        HOST="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    -p|--port)
+        PORT="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    --PASS)
+        pass="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    *)    # unknown option
+        usage
+        shift # past argument
+        exit
+        ;;
+esac
 done
+
+echo "host=$HOST port=$PORT pass=$PASS"
+
+if [ $HOST = "localhost" ] 
+then
+    [ -z "$PORT" ] && PORT=$(docker ps  --format "{{.Image}} {{.Ports}}" | grep "selenium/node" | awk '{print $2}' | sed 's/.*:\([[:digit:]]*\)->.*/\1/')
+
+    for p in $PORT; 
+    do 
+        params="$params HOST $HOST PORT $p PASSWORD $PASS"
+    done
+fi
+
+echo $params
 
 java -jar bin/VncThumbnailViewer.jar $params
